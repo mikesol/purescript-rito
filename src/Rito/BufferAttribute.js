67a94584ef45af5bfb4529df22a3f0bab27b35bf
Mike Solomon <mike120982@gmail.com>
2022-06-18 21:19:07 +0300

    Allows for the creation of multiple buffer attributes

--- src/Rito/BufferAttribute.js
+++ src/Rito/BufferAttribute.js
@@ -1,12 +1,34 @@
 export const bufferAttributeImpl = (m) => (n) => (bufferAttribute) => (f) => {
+	const fa = new Float32Array(m * n);
+	for (let i = 0; i < m; i++) {
+		const o = i * n;
+		const v = f(i);
+		for (let j = 0; j < n; j++) {
+			fa[o + j] = v[j];
+		}
+	}
+	return new bufferAttribute(fa, n);
+};
 
-  const fa = new Float32Array(m*n);
-  for (let i = 0; i < m; i++) {
-    const o = i * n;
-    const v = f(i);
-    for (let j = 0; j < n; j++) {
-      fa[o + j] = v[j];
-    }
-  }
-  return new bufferAttribute(fa, n);
-}
+export const bufferAttributesImpl = (m) => (ns) => (bufferAttribute) => (f) => {
+	const fas = {};
+	for (let i = 0; i < ns.length; i++) {
+		fas[ns[i].k] = new Float32Array(m * ns[i].n);
+	}
+	for (let i = 0; i < m; i++) {
+		const v = f(i);
+		for (let j = 0; j < ns.length; j++) {
+			const key = ns[j].k;
+			const n = ns[j].n;
+			const o = i * n;
+			for (let k = 0; k < n; k++) {
+				fas[key][o + k] = v[key][k];
+			}
+		}
+	}
+	const out = {};
+	for (let i = 0; i < ns.length; i++) {
+		out[ns[i].k] = new bufferAttribute(fas[ns[i].k], ns[i].n);
+	}
+	return out;
+};
--- src/Rito/BufferAttribute.purs
+++ src/Rito/BufferAttribute.purs
@@ -1,8 +1,13 @@
-module Rito.BufferAttribute where
+module Rito.BufferAttribute (bufferAttribute, bufferAttributes, class BufferAttributes, internalDoNotUseBA, BufferAttribute) where
 
+import Data.Array as Array
 import Data.FastVect.FastVect as Vect
 import Data.Reflectable (class Reflectable, reflectType)
+import Data.Symbol (class IsSymbol, reflectSymbol)
+import Data.Tuple (fst)
+import Data.Tuple.Nested (type (/\), (/\))
 import Prim.Int (class Mul)
+import Prim.RowList as RL
 import Rito.THREE as THREE
 import Type.Proxy (Proxy(..))
 
@@ -27,3 +32,43 @@ bufferAttribute
   -> BufferAttribute
 bufferAttribute px = bufferAttributeImpl (reflectType px)
   (reflectType (Proxy :: _ n))
+
+class
+  BufferAttributes (i :: RL.RowList Type) (o :: RL.RowList Type)
+  | i -> o where
+  internalDoNotUseBA :: Proxy i -> (Array {k :: String, n :: Int}) /\ Proxy o
+
+instance BufferAttributes RL.Nil RL.Nil where
+  internalDoNotUseBA _ = [] /\ Proxy
+
+instance
+  ( BufferAttributes restI restO
+  , IsSymbol i
+  , Reflectable n Int
+  ) =>
+  BufferAttributes (RL.Cons i (Vect.Vect n Number) restI)
+    (RL.Cons i BufferAttribute restO) where
+  internalDoNotUseBA _ = Array.cons {k: reflectSymbol (Proxy :: _ i), n: reflectType (Proxy :: _ n)} tail /\ Proxy
+    where
+    tail /\ _ = internalDoNotUseBA (Proxy :: _ restI)
+
+foreign import bufferAttributesImpl
+  :: forall i o
+   . Int
+  -> Array { k :: String, n :: Int }
+  -> THREE.TBufferAttribute
+  -> (Int -> { | i })
+  -> { | o }
+
+bufferAttributes ::
+    forall m i ir o or
+     . Reflectable m Int
+    => RL.RowToList i ir
+    => RL.RowToList o or
+    => BufferAttributes ir or
+    => Proxy m
+    -> THREE.TBufferAttribute
+    -> (Int -> { | i })
+    -> { | o }
+bufferAttributes px = bufferAttributesImpl (reflectType px)
+  (fst (internalDoNotUseBA (Proxy :: _ ir)))
