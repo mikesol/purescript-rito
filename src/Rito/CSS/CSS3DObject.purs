3708959f2f29c75fc2e0f7e70957b9d609543d1e
Mike Solomon <mike120982@gmail.com>
2022-09-01 18:56:40 +0300

    Gets rid of kludge

--- src/Rito/CSS/CSS3DObject.purs
+++ src/Rito/CSS/CSS3DObject.purs
@@ -8,18 +8,14 @@ import Prelude
 
 import Bolson.Core as Bolson
 import Control.Alt ((<|>))
-import Control.Monad.ST (ST)
-import Control.Monad.ST.Global as Region
+import Control.Monad.ST.Class (liftST)
 import Data.Newtype (class Newtype)
 import Data.Variant (Variant, match)
 import Deku.Core (ANut(..))
 import Deku.Toplevel (runInElement')
-import Effect (Effect)
-import FRP.Event (Event, makeLemmingEvent)
+import FRP.Event (Event, makeEvent, subscribe)
 import Rito.Core as C
 import Rito.THREE as THREE
-import Unsafe.Coerce (unsafeCoerce)
-import Web.DOM (Element)
 import Web.DOM.Document (createElement)
 import Web.HTML (window)
 import Web.HTML.HTMLDocument (toDocument)
@@ -47,14 +43,12 @@ css3DObject ipt@{ nut: ANut nut } atts = Bolson.Element' $ C.CSS3DObject go
           , deleteFromCache
           , makeCSS3DObject
           }
-      ) = makeLemmingEvent \mySub k -> do
-    me <- ids
-    parent.raiseId me
-    --- UGGGGHHHHH
-    elt <- (unsafeCoerce :: Effect Element -> ST Region.Global Element) (window >>= document >>= createElement "div" <<< toDocument)
-    --- UGGGGHHHH
-    dku <- ((unsafeCoerce :: Effect (Effect Unit) -> ST Region.Global (ST Region.Global Unit)) (runInElement' elt nut))
-    unsub <- mySub
+      ) = makeEvent \k -> do
+    me <- liftST ids
+    liftST $ parent.raiseId me
+    elt <- window >>= document >>= createElement "div" <<< toDocument
+    dku <- runInElement' elt nut
+    unsub <- subscribe
       ( pure
           ( makeCSS3DObject
               { id: me
